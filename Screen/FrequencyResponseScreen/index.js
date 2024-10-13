b14f253b0328af9178c528bdcebdc909a1df59ec
Lohan Batista Moreira <90569097+Lohan1303@users.noreply.github.com>
2024-10-13 19:50:18 -0300

    Onda dente de serra
    Obs: Não funcionou :(

--- Screen/FrequencyResponseScreen/index.js
+++ Screen/FrequencyResponseScreen/index.js
@@ -1,34 +1,98 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { Text, View, TouchableOpacity } from "react-native";
 import { useEffect } from "react";
+import * as ScreenOrientation from 'expo-screen-orientation';
 import styles from "./styles.js";
+import React, { useState } from 'react';
+import { LineChart } from 'react-native-chart-kit';
 
 export default function FrequencyResponseScreen({ navigation }) {
   useEffect(() => {
+    ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.LANDSCAPE);
+    geraOndaDenteSerra();
+
     console.log(
-      "Entrando na Tela para apresentação do gráfico de resposta em frequência do canal de comunicações (módulo e fase)"
+      "Entrando na Tela para apresentação do gráfico do espectro do sinal de entrada (amplitude e fase de cada componente senoidal)"
     );
     return () => {
       console.log(
-        "Finalizando tela: Tela para apresentação do gráfico de resposta em frequência do canal de comunicações (módulo e fase)"
+        "Finalizando tela: Tela para apresentação do gráfico do espectro do sinal de entrada (amplitude e fase de cada componente senoidal)"
       );
     };
   }, []);
 
+    //Seguindo o jupyter notebook "Geração do Sinal Emitido"
+    const[intervaloInicial,setIntervaloInicial] = useState(-3); // t0
+    const[intervaloFinal,setIntervaloFinal] = useState(3); //tf
+    const[passo,setPasso]= useState(0.4);  //passo
+    const[frequenciaFundamental, setFrequenciaFundamental] = useState(1); //f0
+    const[coordX,setCoordX] = useState([]); //coordenada de X
+    const[coordY,setCoordY] = useState([]); //coordenada de Y
+    const[qtdHarmonicas,setQtdHarmonicas] = useState(100);
+  
+    const geraOndaDenteSerra = () => { // não estou levando em consideração a fase
+      try {
+        let A_n;
+        for (let t = intervaloInicial; t <= intervaloFinal; t += passo) {
+          let somaHarmonicas = 0;
+    
+          for (let n = 1; n <= qtdHarmonicas; n++) {
+            
+            A_n = 2 / (n * Math.PI);
+
+            somaHarmonicas += A_n * Math.cos(2 * Math.PI * n * frequenciaFundamental * t);
+          }
+          coordX.push(t);
+          coordY.push(somaHarmonicas);
+        }
+      } catch (e) {
+        console.log("Erro:", e);
+      } finally {
+        console.log("Coordenadas de X:", coordX);
+        console.log("Coordenadas de Y:", coordY);
+      }
+    };
+    
+    
+
   return (
     <View style={styles.container}>
-      <Text style={styles.texto}>
-        Você está na Tela do gráfico de resposta em frequência do canal de
-        comunicações!
-      </Text>
-      <Text></Text>
-      <Text></Text>
-      <TouchableOpacity
+      <View>
+    {/* Documentação: https://www.npmjs.com/package/react-native-chart-kit */}
+    <LineChart
+          data={{
+            labels: coordX.map(String), 
+            datasets: [
+              {
+                data: coordY,
+              },
+            ],
+          }}
+          width={800}  
+          height={300} 
+          withVerticalLabels = {false} //Acho que através desse atributo é possível filtrar os labels 
+          chartConfig={{
+            backgroundColor: '#ffffff',
+            backgroundGradientFrom: '#ffffff',
+            backgroundGradientTo: '#ffffff',
+            color: (opacity = 1) => `rgba(0, 0, 255, ${opacity})`,
+            labelColor: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+            style: {
+              borderRadius: 16,
+            },
+          }}
+          style={{
+            marginVertical: 8,
+
+          }}
+        />
+    </View>
+      {/* <TouchableOpacity
         style={styles.botao}
         onPress={() => navigation.navigate("HomePage")}
       >
         <Text style={styles.texto}>Voltar para a Home</Text>
-      </TouchableOpacity>
+      </TouchableOpacity> */}
       <StatusBar style="auto" />
     </View>
   );
