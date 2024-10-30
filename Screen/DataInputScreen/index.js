c985b4147f5812c46819fd90b466b26d6a158db1
Lohan1303 <lohanbatista2003@gmail.com>
2024-10-29 22:37:45 -0300

    Ajuste labels do grafico

--- Screen/DataInputScreen/index.js
+++ Screen/DataInputScreen/index.js
@@ -28,13 +28,13 @@ export default function DataImputScreen({ navigation }) {
   }, []);
 
   //Seguindo o jupyter notebook "Geração do Sinal Emitido"
-  const [intervaloInicial, setIntervaloInicial] = useState(-3); // t0
+  const [intervaloInicial, setIntervaloInicial] = useState(0); // t0
   const [intervaloFinal, setIntervaloFinal] = useState(3); //tf
   const [passo, setPasso] = useState(0.01); //passo
-  const [frequenciaFundamental, setFrequenciaFundamental] = useState(1); //f0
+  const [frequenciaFundamental, setFrequenciaFundamental] = useState(5); //f0
   const [coordX, setCoordX] = useState([]); //coordenada de X
   const [coordY, setCoordY] = useState([]); //coordenada de Y
-  const[qtdHarmonicas,setQtdHarmonicas] = useState(300);
+  const [qtdHarmonicas, setQtdHarmonicas] = useState(300);
 
   const geraOndaQuadrada = () => {
     try {
@@ -44,8 +44,16 @@ export default function DataImputScreen({ navigation }) {
         const coordY = Math.sign(
           Math.sin(2 * Math.PI * frequenciaFundamental * t)
         );
+
         setCoordY((prevCoordY) => [...prevCoordY, coordY]);
-        setCoordX((prevCoordx) => [...prevCoordx, t]);
+        if (
+          coordY >
+          Math.sign(Math.sin(2 * Math.PI * frequenciaFundamental * (t + passo)))
+        ) {
+          setCoordX((prevCoordx) => [...prevCoordx, t.toFixed(2)]);
+        } else {
+          setCoordX((prevCoordx) => [...prevCoordx, ""]);
+        }
       }
     } catch (e) {
       console.log("Erro:", e);
@@ -55,57 +63,63 @@ export default function DataImputScreen({ navigation }) {
     }
   };
 
- const geraOndaDenteSerra = () => { // não estou levando em consideração a fase
-    try {
-      setCoordY([]);
-      setCoordX([]);
-      let A_n;
-      for (let t = intervaloInicial; t <= intervaloFinal; t += passo) {
-      let somaHarmonicas = 0;
-
-      for (let n = 1; n <= qtdHarmonicas; n++) {
-
-        //let A_n = (2 * Math.pow(-1, n + 1)) / n;
-         let A_n = 2 / (Math.PI * n);
-        somaHarmonicas += -A_n * Math.sin(2 * Math.PI * n * frequenciaFundamental * t);
-      }
-
-      // Armazenando os valores em X e Y
-      setCoordY((prevCoordY) => [...prevCoordY, somaHarmonicas]);
-      if(somaHarmonicas < 1 && somaHarmonicas > 0.9)
-      {
-        setCoordX((prevCoordX) => [...prevCoordX, Math.round(t)]);
-      }
-      else {
-        setCoordX((prevCoordX) => [...prevCoordX, '']);
-      }
-    }
-    } catch (e) {
-      console.log("Erro:", e);
-    } finally {
-      console.log("Coordenadas de X:", coordX);
-      console.log("Coordenadas de Y:", coordY);
-    }
-  };
-
-  const geraOndaTriangular = () => { //não estou levando em consideração a fase
+  const geraOndaDenteSerra = () => {
+    // não estou levando em consideração a fase
     try {
       setCoordY([]);
       setCoordX([]);
       let A_n;
       for (let t = intervaloInicial; t <= intervaloFinal; t += passo) {
         let somaHarmonicas = 0;
-  
-        for (let n = 1; n <= qtdHarmonicas; n ++) {
-          if(n % 2 != 0){
-            A_n = 8 / (Math.pow(Math.PI, 2) * Math.pow(n, 2));
-          }
-          else{
-            A_n = 0;
-          }   
-          somaHarmonicas += A_n * Math.cos(2 * Math.PI * n * t );
+        let somaHarmonicas_1 = 0;
+        let somaHarmonicas_2 = 0;
+        
+        for (let n = 1; n <= qtdHarmonicas; n++) {
+          //let A_n = (2 * Math.pow(-1, n + 1)) / n;
+          let A_n = 2 / (Math.PI * n);
+          let A_n_1 = 2 / (Math.PI * n - 1);
+          let A_n_2 = 2 / (Math.PI * n + 1);
+
+          somaHarmonicas += -A_n * Math.sin(2 * Math.PI * n * frequenciaFundamental * t);
+          somaHarmonicas_1 += -A_n_1 * Math.sin(2 * Math.PI * n * frequenciaFundamental * (t - passo));
+          somaHarmonicas_2 += -A_n_2 * Math.sin(2 * Math.PI * n * frequenciaFundamental * (t + passo));
         }
-  
+
+        // Armazenando os valores em X e Y
+        setCoordY((prevCoordY) => [...prevCoordY, somaHarmonicas]);
+
+        if (somaHarmonicas_1 > somaHarmonicas && somaHarmonicas <= somaHarmonicas_2) {
+          setCoordX((prevCoordX) => [...prevCoordX, (t + passo).toFixed(2)]);
+        } else {
+          setCoordX((prevCoordX) => [...prevCoordX, ""]);
+        }
+      }
+    } catch (e) {
+      console.log("Erro:", e);
+    } finally {
+      console.log("Coordenadas de X:", coordX);
+      console.log("Coordenadas de Y:", coordY);
+    }
+  };
+
+  const geraOndaTriangular = () => {
+    //não estou levando em consideração a fase
+    try {
+      setCoordY([]);
+      setCoordX([]);
+      let A_n;
+      for (let t = intervaloInicial; t <= intervaloFinal; t += passo) {
+        let somaHarmonicas = 0;
+
+        for (let n = 1; n <= qtdHarmonicas; n++) {
+          if (n % 2 != 0) {
+            A_n = 8 / (Math.pow(Math.PI, 2) * Math.pow(n, 2));
+          } else {
+            A_n = 0;
+          }
+          somaHarmonicas += A_n * Math.cos(2 * Math.PI * n * t);
+        }
+
         setCoordY((prevCoordY) => [...prevCoordY, somaHarmonicas]);
         setCoordX((prevCoordx) => [...prevCoordx, t]);
       }
@@ -116,13 +130,15 @@ export default function DataImputScreen({ navigation }) {
       console.log("Coordenadas de Y:", coordY);
     }
   };
-  
-  geraOndaSenoidalRetificada =() => {
+
+  geraOndaSenoidalRetificada = () => {
     try {
       setCoordY([]);
       setCoordX([]);
       for (let t = intervaloInicial; t <= intervaloFinal; t += passo) {
-        const coordY = Math.abs(Math.sin(2 * Math.PI * frequenciaFundamental * t)) ;
+        const coordY = Math.abs(
+          Math.sin(2 * Math.PI * frequenciaFundamental * t)
+        );
 
         setCoordY((prevCoordY) => [...prevCoordY, coordY]);
         setCoordX((prevCoordx) => [...prevCoordx, t]);
@@ -134,7 +150,6 @@ export default function DataImputScreen({ navigation }) {
       console.log("Coordenadas de Y:", coordY);
     }
   };
-  
 
   return (
     <View style={styles.container}>
@@ -167,11 +182,8 @@ export default function DataImputScreen({ navigation }) {
             }}
             withInnerLines={true}
             withVerticalLabels={true} //Acho que através desse atributo é possível filtrar os labels
-          >
-          </LineChart>
-            <View style={{ borderColor: 2, border: 2, width: 100 }}>
-
-            </View>
+          ></LineChart>
+          <View style={{ borderColor: 2, border: 2, width: 100 }}></View>
         </View>
       )}
 
