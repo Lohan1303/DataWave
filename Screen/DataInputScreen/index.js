8eb47e5ee387b358ff6da8232dd723835b2c269d
Lohan1303 <lohanbatista2003@gmail.com>
2024-11-08 20:47:00 -0300

    Filtros feitos com sucesso de todos os sinais de entrada

--- Screen/DataInputScreen/index.js
+++ Screen/DataInputScreen/index.js
@@ -15,9 +15,9 @@ export default function DataImputScreen({ navigation }) {
       "Entrando na Tela de entrada de dados para efetuar os cálculos"
     );
 
-    // geraOndaSenoidalRetificada();
+    geraOndaSenoidalRetificada();
     // geraOndaDenteSerra();
-    geraOndaTriangular();
+    // geraOndaTriangular();
     // geraOndaQuadrada();
     return () => {
       console.log(
@@ -29,10 +29,10 @@ export default function DataImputScreen({ navigation }) {
   }, []);
 
   //Seguindo o jupyter notebook "Geração do Sinal Emitido"
-  const [intervaloInicial, setIntervaloInicial] = useState(-10); // t0
-  const [intervaloFinal, setIntervaloFinal] = useState(5); //tf
+  const [intervaloInicial, setIntervaloInicial] = useState(-2); // t0
+  const [intervaloFinal, setIntervaloFinal] = useState(2); //tf
   const [passo, setPasso] = useState(0.01); //passo
-  const [frequenciaFundamental, setFrequenciaFundamental] = useState(0.5); //f0
+  const [frequenciaFundamental, setFrequenciaFundamental] = useState(1); //f0
   const [coordX, setCoordX] = useState([]); //coordenada de X
   const [coordY, setCoordY] = useState([]); //coordenada de Y
   const [qtdHarmonicas, setQtdHarmonicas] = useState(300);
@@ -74,22 +74,30 @@ export default function DataImputScreen({ navigation }) {
         let somaHarmonicas = 0;
         let somaHarmonicas_1 = 0;
         let somaHarmonicas_2 = 0;
-        
+
         for (let n = 1; n <= qtdHarmonicas; n++) {
           //let A_n = (2 * Math.pow(-1, n + 1)) / n;
           let A_n = 2 / (Math.PI * n);
           let A_n_1 = 2 / (Math.PI * n - 1);
           let A_n_2 = 2 / (Math.PI * n + 1);
 
-          somaHarmonicas += -A_n * Math.sin(2 * Math.PI * n * frequenciaFundamental * t);
-          somaHarmonicas_1 += -A_n_1 * Math.sin(2 * Math.PI * n * frequenciaFundamental * (t - passo));
-          somaHarmonicas_2 += -A_n_2 * Math.sin(2 * Math.PI * n * frequenciaFundamental * (t + passo));
+          somaHarmonicas +=
+            -A_n * Math.sin(2 * Math.PI * n * frequenciaFundamental * t);
+          somaHarmonicas_1 +=
+            -A_n_1 *
+            Math.sin(2 * Math.PI * n * frequenciaFundamental * (t - passo));
+          somaHarmonicas_2 +=
+            -A_n_2 *
+            Math.sin(2 * Math.PI * n * frequenciaFundamental * (t + passo));
         }
 
         // Armazenando os valores em X e Y
         setCoordY((prevCoordY) => [...prevCoordY, somaHarmonicas]);
 
-        if (somaHarmonicas_1 > somaHarmonicas && somaHarmonicas <= somaHarmonicas_2) {
+        if (
+          somaHarmonicas_1 > somaHarmonicas &&
+          somaHarmonicas <= somaHarmonicas_2
+        ) {
           setCoordX((prevCoordX) => [...prevCoordX, (t + passo).toFixed(2)]);
         } else {
           setCoordX((prevCoordX) => [...prevCoordX, ""]);
@@ -109,9 +117,10 @@ export default function DataImputScreen({ navigation }) {
       setCoordY([]);
       setCoordX([]);
       let A_n;
+      
       for (let t = intervaloInicial; t <= intervaloFinal; t += passo) {
         let somaHarmonicas = 0;
-        const periodo = 1 / ( frequenciaFundamental); // Cálculo do período da onda
+        const periodo = 1 / frequenciaFundamental; // Cálculo do período da onda
         for (let n = 1; n <= qtdHarmonicas; n++) {
           if (n % 2 != 0) {
             A_n = 8 / (Math.pow(Math.PI, 2) * Math.pow(n, 2));
@@ -122,12 +131,10 @@ export default function DataImputScreen({ navigation }) {
         }
 
         setCoordY((prevCoordY) => [...prevCoordY, somaHarmonicas]);
-        if((Math.abs(t) % periodo) < passo)
-        {
+        if (Math.abs(t).toFixed(5) % periodo < passo) {
           setCoordX((prevCoordx) => [...prevCoordx, t.toFixed(1)]);
-        }
-        else{
-          setCoordX((prevCoordx) => [...prevCoordx, '']);
+        } else {
+          setCoordX((prevCoordx) => [...prevCoordx, ""]);
         }
       }
     } catch (e) {
@@ -138,21 +145,22 @@ export default function DataImputScreen({ navigation }) {
     }
   };
 
-  geraOndaSenoidalRetificada = () => {
+  const geraOndaSenoidalRetificada = () => {
     try {
-      setCoordY([]);
-      setCoordX([]);
-      const meio_periodo = 1 / (2 * frequenciaFundamental); // Cálculo do período da onda
+      setCoordY([]); // Limpa o array de Y
+      setCoordX([]); // Limpa o array de X
+  
+      let cond = false; // Flag para capturar o primeiro zero positivo
+      const meio_periodo = 1 / (2 * frequenciaFundamental); // Cálculo do meio período
+  
       for (let t = intervaloInicial; t <= intervaloFinal; t += passo) {
-        const coordY = Math.abs(
-          Math.sin(2 * Math.PI * frequenciaFundamental * t)
-        );
-        setCoordY((prevCoordY) => [...prevCoordY, coordY]);
-        if((Math.abs(t) % meio_periodo) < passo)
-        {
+        const coordY = Math.abs(Math.sin(2 * Math.PI * frequenciaFundamental * t)); // Cálculo de Y
+        setCoordY((prevCoordY) => [...prevCoordY, coordY]); // Atualiza o array de coordenadas Y
+  
+        
+        if (Math.abs(t).toFixed(4) % meio_periodo < passo) {
           setCoordX((prevCoordx) => [...prevCoordx, t.toFixed(1)]);
-        }
-        else{
+        } else {
           setCoordX((prevCoordx) => [...prevCoordx, '']);
         }
       }
@@ -163,6 +171,7 @@ export default function DataImputScreen({ navigation }) {
       console.log("Coordenadas de Y:", coordY);
     }
   };
+  
 
   return (
     <View style={styles.container}>
